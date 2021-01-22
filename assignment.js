d6fd833cec9e02f7761de906677b6816b57c27dc
udaymondal <udaysk.mondal@gmail.com>
2021-01-22 21:55:49 +0600

    2done

--- assignment.js
+++ assignment.js
@@ -2,7 +2,8 @@
 
 // Problem 1:
 
-function kilometerToMeter(kilometer){
+function kilometerToMeter(kilometer)
+{
     if(kilometer >= 0){
         var meter = kilometer * 1000 ;
         return meter;
@@ -12,4 +13,26 @@ function kilometerToMeter(kilometer){
     }
 }
 
-console.log(kilometerToMeter(-595));
+//console.log(kilometerToMeter(-595));
+
+// Problem 2:
+
+function budgetCalculator(numberOfWatch, numberOfMobile, numberOfLaptop)
+{
+    if(numberOfWatch >=0 && numberOfMobile>= 0 && numberOfLaptop>= 0)
+    {
+        var watchPrice = 50 * numberOfWatch;
+        var mobilePrice = 100 * numberOfMobile;
+        var laptopPrice = 500 * numberOfLaptop;
+        var totalCost = watchPrice + mobilePrice + laptopPrice;
+        return totalCost;
+    }
+    else{
+        return ("Enter valid numbers");
+    }  
+}
+
+//console.log(budgetCalculator(10, 5, 3));
+
+
+
